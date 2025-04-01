a0ec0f4ba8c872833081c3fceefb4d9bb8447fdb
Surya <157574400+0710surya@users.noreply.github.com>
2025-04-02 01:07:46 +0530

    Added subject in mail

--- app/Http/Controllers/Front/ContactFormEntryController.php
+++ app/Http/Controllers/Front/ContactFormEntryController.php
@@ -47,6 +47,7 @@ class ContactFormEntryController extends Controller
                 'request' => $request->all(),
             ], function ($mail) use ($request) {
                 $mail->from('info@3elabs.com', 'Info');
+                $mail->subject('New Enquiry');
                 $mail->to('suryakantyadav16@gmail.com')->subject($request->subject);
             });
 
